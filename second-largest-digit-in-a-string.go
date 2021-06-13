0022f9c7c96e7df6da40bf71a444f5fe202d42ff
Yuji Nishimura <nishimura.yuji@liclab.com>
2021-06-13 21:45:10 +0900

    stringへの変換は不要だった

--- second-largest-digit-in-a-string.go
+++ second-largest-digit-in-a-string.go
@@ -3,9 +3,9 @@ package leetcode
 func secondHighest(s string) int {
 	h1 := -1
 	h2 := -1
-	a2i := map[string]int{"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+	a2i := map[rune]int{'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 	for _, r := range s {
-		i, ok := a2i[string(r)]
+		i, ok := a2i[r]
 		if !ok {
 			continue
 		}
